860fe9d8ee8287ba33bb823eb04aa213e8227a1e
Vitaliy <theyamshikov@yandex.ru>
2022-07-06 18:08:09 +0600

    upd

---
+++ src/Helpers/FilterHelper.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ZnCore\Arr\Helpers;
+
+use ZnCore\Collection\Libs\Collection;
+use ZnCore\Entity\Helpers\CollectionHelper;
+use ZnCore\Query\Entities\Query;
+
+class FilterHelper
+{
+
+    public static function filterItems(array $items, Query $query): array
+    {
+        $collection = new Collection($items);
+        $collection = CollectionHelper::filterByQuery($collection, $query);
+        return $collection->toArray();
+
+//        return $collection->matching($criteria);
+
+
+//        $collection = new Collection($items);
+//        /** @var Where[] $whereArray */
+////        $whereArray = $query->getParam(Query::WHERE_NEW);
+//        $whereArray = $query->getWhere();
+//        if ($whereArray) {
+//            $collection = CollectionHelper::whereArr($collection, $whereArray);
+////            $collection = self::filterItemsByCondition($collection, $whereArray);
+//        }
+//        return $collection->toArray();
+    }
+
+    /*private static function filterItemsByCondition(Enumerable $collection, array $whereArray): Collection
+    {
+        $collection = new \Illuminate\Support\Collection($collection->toArray());
+        foreach ($whereArray as $where) {
+            $values = ArrayHelper::toArray($where->value);
+            $resultCollection = new \Illuminate\Support\Collection();
+            foreach ($values as $value) {
+                $filteredCollection = $collection->where($where->column, $where->operator, $value);
+                $resultCollection = $resultCollection->concat($filteredCollection);
+            }
+            $collection = $resultCollection;
+        }
+
+        return new Collection($collection->toArray());
+//        return $collection;
+    }*/
+}
